48bd4c5e2036ddd5c0ed3a44359129823afccac2
emerz35 <charlie.h.hands@gmail.com>
2018-07-13 21:49:11 +0100

    Changed names

--- motivision-games-app/src/motivision/games/app/Main.java
+++ motivision-games-app/src/motivision/games/app/Main.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package motivision.games.app;
 
 /**
  *
- * @author charl
+ * @author Charlie Hands
  */
 public class Main {
 
@@ -15,7 +10,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
